b3fc7bb14c997139b322d9862abe828700acae95
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 20:07:34 +0200

    refactor(apps): move settings into apps

---
+++ src/apps/Settings.jsx
@@ -0,0 +1,3 @@
+export default function Settings() {
+ return <div className="settings-list"><div><b>rainOS</b><span>Tablet 1.0 · build Tablet-1000</span></div><div><b>Wi‑Fi</b><span>Conectado</span></div><div><b>Bluetooth</b><span>Activado</span></div><div><b>Aspecto</b><span>Automático</span></div></div>;
+}
