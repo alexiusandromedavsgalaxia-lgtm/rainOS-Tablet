14b20d998f7eb1bb0b107adab3af5cf45c75b94d
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 21:46:14 +0200

    Add SVG Repo icon source catalog

---
+++ src/ui/svgRepoIcons.js
@@ -0,0 +1,12 @@
+// SVG Repo assets selected for rainOS Tablet.
+// Sources are CC0 assets listed by SVG Repo. Keep source URLs with the mapping.
+export const svgRepoIcons = {
+  calendar: 'https://www.svgrepo.com/download/377201/calendar-grid.svg',
+  mail: 'https://www.svgrepo.com/download/2784/mail.svg',
+  messages: 'https://www.svgrepo.com/download/376421/chat-2-text-line.svg',
+  safari: 'https://www.svgrepo.com/download/302428/compass.svg',
+  music: 'https://www.svgrepo.com/download/48423/music.svg',
+  files: 'https://www.svgrepo.com/download/60011/document-folder.svg',
+  settings: 'https://www.svgrepo.com/download/205145/settings-gear.svg',
+  camera: 'https://www.svgrepo.com/download/23887/camera-outline.svg',
+};
