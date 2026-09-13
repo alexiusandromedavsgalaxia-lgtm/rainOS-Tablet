8ec3efdbe0322a6a469d402342ec2a4e5d55e1b6
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 20:25:21 +0200

    Add kernel exports

---
+++ src/kernel/index.js
@@ -0,0 +1,4 @@
+export { default as Kernel, KernelState } from './Kernel.js';
+export { default as Scheduler } from './Scheduler.js';
+export { default as MemoryManager } from './MemoryManager.js';
+export { default as ProcessManager } from './ProcessManager.js';
