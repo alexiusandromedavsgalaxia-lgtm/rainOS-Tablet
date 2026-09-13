9d7a9f51b250e5c1b0246f72d6d7b88094c1fe22
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 20:25:38 +0200

    Add UI exports

---
+++ src/ui/index.js
@@ -0,0 +1,5 @@
+export { default as AppIcon } from './AppIcon.jsx';
+export { default as AppWindow } from './AppWindow.jsx';
+export { default as ControlCenter } from './ControlCenter.jsx';
+export { default as Dock } from './Dock.jsx';
+export { default as StatusBar } from './StatusBar.jsx';
