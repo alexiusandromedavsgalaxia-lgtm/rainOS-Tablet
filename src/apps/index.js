ea7edb1540072cd5738159cb7086680e5268602e
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-13 20:25:33 +0200

    Add application exports

---
+++ src/apps/index.js
@@ -0,0 +1,4 @@
+export { apps, getApp, getAppsByCategory, getSystemApps, isInstalled } from './appRegistry.js';
+export { default as Calculator } from './Calculator.jsx';
+export { default as Settings } from './Settings.jsx';
+export { Files, Notes, Photos, Safari, Weather } from './SystemApps.jsx';
